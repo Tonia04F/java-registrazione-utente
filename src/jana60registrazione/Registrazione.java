b10c83b2044b0b066da5c1ddab56375f6d04b88f
tonia <tonia@cia.como>
2022-06-24 17:01:46 +0200

    fine esercizio

--- src/jana60registrazione/Registrazione.java
+++ src/jana60registrazione/Registrazione.java
@@ -1,10 +1,49 @@
 package jana60registrazione;
 
+import java.util.Scanner;
+
 public class Registrazione {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*Creare una classe Registrazione con metodo main in cui, tramite Scanner, vengono chieste le informazioni all’utente e
+		 *  si prova a creare un nuovo oggetto Utente.
+		Gestire tramite try e catch eventuali errori in fase di creazione in modo che il programma non si interrompa 
+		bruscamente ma chieda all’utente di inserire dei dati corretti.*/
 
+		Scanner sc = new Scanner(System.in);
+		
+		boolean ritenta = true;
+		
+		do {
+			System.out.println("inserisci nome: ");
+			String nome = sc.nextLine();
+			System.out.println("inserisci cognome: ");
+			String cognome = sc.nextLine();
+			
+			try {
+				System.out.println("inserisci email: ");
+				String email = sc.nextLine();
+				System.out.println("Inserisci password:");
+				String password = sc.nextLine();
+				System.out.println("inserisci eta: ");
+				int eta = Integer.parseInt(sc.nextLine());
+
+				Utente nuovoUtente = new Utente(nome, cognome, email, password, eta);
+				System.out.println("Grazie per esserti registrato");
+				System.out.println(nuovoUtente);
+				 ritenta = false;
+
+			} catch (NumberFormatException etaUtente) {
+				System.out.println("l'utente deve avere almeno 18 anni");
+				System.out.println(etaUtente.getMessage());
+				System.out.println("ritenta");
+			} catch (Exception pass) {
+				System.out.println("i dati inseriti non sono validi si prega di ritentare");
+				System.out.println(pass.getMessage());
+				System.out.println("ritenta");
+			} 
+		} while (ritenta);
+sc.close();
 	}
 
 }
--- src/jana60registrazione/Utente.java
+++ src/jana60registrazione/Utente.java
@@ -1,5 +1,115 @@
 package jana60registrazione;
 
 public class Utente {
+/*Creare una classe Utente con attributi nome, cognome, email, password ed età.
+Aggiungere un costruttore con parametri e metodi getter e setter.
+Implementare dei metodi di validazione in modo che:
+- email contenga un carattere @ e un .
+- password sia lunga tra 8 e 12 caratteri
+- età sia maggiore di 18 anni
+In caso i parametri non siano validi, impedire che vengano valorizzati gli attributi nei metodi e nel costruttore, 
+sollevando opportune eccezioni.*/
+	
+	private String nome;
+	private String cognome;
+	private String email;
+	private String password;
+	private int eta;
+	
+//costruttore
+	public Utente(String nome, String cognome, String email, String password, int eta) throws Exception {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.password = password;
+		this.eta = eta;
+		this.emailValida();
+		this.passwordValida();
+		this.etaValida();
+	}
 
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public int getEta() {
+		return eta;
+	}
+
+
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+	private void emailValida() throws Exception {
+		//controllo che la mail sia correttamente formattata
+				if(!(email.contains("@")) || !(email.contains("."))) {
+					throw new Exception ("email non valida inserire @");
+				}
+	}
+	private void passwordValida() throws Exception {
+		//controllo che la mail sia correttamente formattata
+				if(password.length()<8 || password.length()>12) {
+					throw new Exception ("password non valida, deve contenere un minimo di 8 caratteri e un massimo di 12 caratteri");
+				}
+	}
+	private void etaValida() throws Exception{
+		//faccio lo stesso controllo per l eta 
+				if( eta < 18 && eta >120) {
+					//altrimenti metto eccezione
+					throw new Exception("età passeggero non valida");
+				}
+	}
+	 @Override
+	    public String toString() {
+	        return "Utente: " + nome + " " + cognome + "  " + eta + " Email: " + email +
+	                " password: " + password + ". Grazie per esserti registrato";
+	    }
+	
 }
